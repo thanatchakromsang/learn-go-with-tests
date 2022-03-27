46f6bbcae1a646ed3fabf4520aaadcf99350a489
Thanatchaya Kromsaeng <thanatchakromsang@gmail.com>
2022-03-27 14:16:33 +0700

    iteration

---
+++ 2.iteration/repeat_test.go
@@ -0,0 +1,37 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Repeat(character string, repeatCount int) string {
+	var repeated string
+
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	}
+
+	return repeated
+}
+
+func ExampleRepeat() {
+  repeated := Repeat("b", 10)
+  fmt.Println(repeated)
+  // Output: bbbbbbbbbb
+}
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
